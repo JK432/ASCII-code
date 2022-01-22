40f99dc7f51a2f36df1e09c9b9207b7c1b5ed84a
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-22 05:10:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# Assume that the variable testString input a string from user. Write a loop that prints each character in this string, followed by its ASCII code
+x=input()
+for i in x:
+  print(i,ord(i))
